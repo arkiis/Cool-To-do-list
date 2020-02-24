399aa729ddac02d0dca416d057e8b77d74003baa
Your Name <william.fleckenstein@gmail.com>
2020-02-23 19:45:19 -0600

    fixed changes

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { FaPizzaSlice } from "react-icons/fa";
-import { Logo } from "../../assets/profile-icon-grey.jpg";
+
 import { AddTask } from "../AddTask";
 
 const Header = ({ darkMode, setDarkMode }) => {
@@ -11,7 +11,7 @@ const Header = ({ darkMode, setDarkMode }) => {
     <header className="header" data-testid="header">
       <nav>
         <div className="logo">
-          <img src="../../assets/profile-icon-grey.jpg" alt="todoLogo" />
+          <img src="/image/logo.png" alt="Todoist" />
         </div>
         <div className="settings">
           <ul>
