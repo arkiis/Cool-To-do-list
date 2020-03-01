89fbd12831c2736ed53216c92180a211677eff7b
Your Name <william.fleckenstein@gmail.com>
2020-03-01 00:00:27 -0600

    conditional routes set up

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
-import React, { useState } from "react";
+import React, { Suspense } from "react";
 import Header from "./components/layout/Header";
 import { NotificationContainer } from "react-notifications";
 import { Route, Redirect, Switch } from "react-router-dom";
 import { connect } from "react-redux";
-
+import Register from "./components/Auth/Register";
 import Content from "./components/layout/Content";
 import { ProjectsProvider, SelectedProjectProvider } from "./context";
 import PropTypes from "prop-types";
 import Layout from "./components/layout";
+import VerifyEmail from "./components/Auth/VerifyEmail";
+import Home from "./pages/Home";
 
 const App = ({ loggedIn, emailVerified }) => {
   let routes;
   if (loggedIn && !emailVerified) {
     routes = (
       <Switch>
-        <Content />
+        <Route exact path="/verify-email" component={VerifyEmail} />
+      </Switch>
+    );
+  } else if (loggedIn && emailVerified) {
+    routes = (
+      <Suspense fallback={<div>Loading...</div>}>
+        <Switch>
+          <Route exact path="/" component={Content} />
+          <Redirect to="/" />
+        </Switch>
+      </Suspense>
+    );
+  } else {
+    routes = (
+      <Switch>
+        <Route exact path="/home" component={Home} />
+        <Route exact path="/signup" component={Register} />
+        <Redirect to="/home" />
       </Switch>
     );
   }
---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>whats up</h1>
+    </div>
+  );
+};
+
+export default Home;
