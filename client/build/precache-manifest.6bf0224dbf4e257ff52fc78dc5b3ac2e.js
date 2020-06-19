84d3ca0d802d79adbfbe56ef5c8c83a0054ea860
Your Name <william.fleckenstein@gmail.com>
2020-06-19 12:20:22 -0500

    dev merged to master

---
+++ client/build/precache-manifest.6bf0224dbf4e257ff52fc78dc5b3ac2e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ed02f0a5c08cc0a70889c0acaaf38c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9bbb093956dd571c0fd6",
+    "url": "/static/css/main.d6b353c2.chunk.css"
+  },
+  {
+    "revision": "a359e25c6d4005d086c7",
+    "url": "/static/js/2.c95482a0.chunk.js"
+  },
+  {
+    "revision": "95e8844530789aaaf2f6e8af37ac45e5",
+    "url": "/static/js/2.c95482a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "349d14ab13bb338cf646",
+    "url": "/static/js/3.b083f695.chunk.js"
+  },
+  {
+    "revision": "69c48e7b7201260b288c",
+    "url": "/static/js/4.29a32d6e.chunk.js"
+  },
+  {
+    "revision": "9bbb093956dd571c0fd6",
+    "url": "/static/js/main.6d209d5f.chunk.js"
+  },
+  {
+    "revision": "34404258503e0930ab15",
+    "url": "/static/js/runtime-main.557abb78.js"
+  }
+]);
